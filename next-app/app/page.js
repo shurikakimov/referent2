b5107876604d5a5fa0b2dcc0b8bfa9628af0f8bb
Shurik <shurik.akimov@gmail.com>
2026-02-23 19:34:31 +0300

    Рефернет

--- next-app/app/page.js
+++ next-app/app/page.js
@@ -1,10 +1,5 @@
 export default function HomePage() {
-  return (
-    <main>
-      <h1>Привет, Next.js!</h1>
-      <p>Это минимальное приложение на Next.js.</p>
-    </main>
-  );
+  return <main>Я изучаю Next.js</main>;
 }
 
 
